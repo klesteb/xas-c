19568738b3c41b8fad6c748a4dbe624e2fb3b45e
Kevin L. Esteb <kevin@kesteb.us>
2024-08-21 17:42:34 -0700

    corrected some macros

--- trunk/xas/misc/bitops.h
+++ trunk/xas/misc/bitops.h
@@ -11,10 +11,10 @@
 
 #define LBOOL(x) (!(!(x)))
 
-#define bit_set(arg,posn) ((arg) | (1L << (posn)))
-#define bit_clear(arg,posn) ((arg) & ~(1L << (posn)))
-#define bit_test(arg,posn) LBOOL((arg) & (1L << (posn)))
-#define bit_flip(arg,posn) ((arg) ^ (1L << (posn)))
+#define bit_set(arg,posn)   ((arg)      |=  (1L << (posn)))
+#define bit_clear(arg,posn) ((arg)      &= ~(1L << (posn)))
+#define bit_flip(arg,posn)  ((arg)      ^=  (1L << (posn)))
+#define bit_test(arg,posn)  LBOOL((arg) &   (1L << (posn)))
 
 #endif
 
